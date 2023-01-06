c26e6beb4e594d8ae4ced9bbce602a09b66f075f
JonataRocha <JonataRocha1611@gmail.com>
2023-01-06 15:52:14 -0300

    ok

--- If_Else.C
+++ If_Else.C
@@ -4,11 +4,14 @@
 //algoritmo que ler três números inteiros e imprime os três em ordem crescente.
 
  main(){
+// 	Declaração de variáveis
  	int num1, num2, num3;
  	
+// 	Entrada de Dados
 	printf("Informe tres numeros inteiros:");
 	scanf("%d %d %d", &num1, &num2, &num3);
 	
+//	Processamento&Saída de dados
 	if(num1<num2 && num1<num3){
 		if(num2<num3)
 			printf("%d %d %d", num1, num2, num3);
